e4e143059eff40f3d1bf239e6c6681343b7fce34
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-08-24 20:18:47 -0300

    📦 NEW: Home interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
+import { FiUser, FiCalendar } from 'react-icons/fi';
+
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Head from 'next/head';
 
 interface Post {
   uid?: string;
@@ -24,9 +28,50 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+      <main className={commonStyles.main}>
+        <div className={styles.posts}>
+          <Link href="/post/1">
+            <a>
+              <h2>Título do Post</h2>
+              <p>Resumo do que fala o post de maneira geral somente para visualização</p>
+              <span>
+                <i><FiCalendar /></i> <time>19 Abr 2022</time>
+                <i><FiUser /></i> Gabriel Albuquerque
+              </span>
+            </a>
+          </Link>
+          <Link href="/post/2">
+            <a>
+              <h2>Título do Post 2</h2>
+              <p>Resumo do que fala o post de maneira geral somente para visualização</p>
+              <span>
+                <i><FiCalendar /></i> <time>13 Jun 2022</time>
+                <i><FiUser /></i> Gabriel Albuquerque
+              </span>
+            </a>
+          </Link>
+          <Link href="/post/2">
+            <a>
+              <h2>Título do Post 3</h2>
+              <p>Resumo do que fala o post de maneira geral somente para visualização</p>
+              <span>
+                <i><FiCalendar /></i> <time>09 Ago 2022</time>
+                <i><FiUser /></i> Gabriel Albuquerque
+              </span>
+            </a>
+          </Link>
+        </div>
+        <span className={styles.loadMore}>Carregar mais posts</span>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
