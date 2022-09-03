80881b123775fb3da7ab83bc45d70d6193108bae
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-09-03 14:12:24 -0300

    📦 NEW:

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,9 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import Head from 'next/head';
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +31,116 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({post}: PostProps) {
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+  // useEffect(() => {
+  //   const body = post.data.content.reduce((acc, cur) => {
+  //     acc.push(cur.body);
 
-//   // TODO
-// };
+  //     return acc;
+  //   }, []);
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  //   console.log(body);
 
-//   // TODO
-// };
+  //   const treatedBody = []; 
+  //   body.forEach(group => {
+  //     group.forEach(p => {
+  //       p === "" && treatedBody.push(p)
+  //     })
+  //   })
+
+  //   console.log(treatedBody)
+
+  // }, [])
+
+  function wordCount() {
+    let groupsArray = post.data.content.map(group => {
+      return group.body;
+    })
+
+    console.log(groupsArray)
+
+    // const paragraphsArray = groupsArray.reduce((acc, curr) => {
+
+    // }, [])
+
+    return '4 min'
+  }
+  
+
+  return (
+    <>
+      <Head>
+        <title>{post.data.title} | spacetraveling</title>
+      </Head>
+      <img className={styles.image} src={post.data.banner.url} alt="" />
+      <main className={styles.main}>
+        <header>
+          <h1>{post.data.title}</h1>
+          <p>
+            <span><i><FiCalendar /></i> <time>{post.first_publication_date}</time></span>
+            <span><i><FiUser /></i> {post.data.author}</span>
+            <span><i><FiClock /></i> {wordCount()}</span>
+          </p>
+        </header>
+
+        <article>
+          {post.data.content.map((hit, index) => (
+            <section key={index}>
+              <strong>{hit?.heading}</strong>
+              {hit.body.map((paragraph, index) => (
+                <p key={index}>{paragraph}</p>
+              ))}
+            </section>
+          ))}
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('post');
+  
+  const slugs = posts.results.map(hit => {
+    return `/post/${hit.slugs[0]}`
+  })
+    
+  return {
+    paths: slugs,
+    fallback: true,
+  }
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('post', slug);
+
+  const post = {
+    first_publication_date: format(new Date(response.first_publication_date), 'PP', { locale: ptBR }),
+    data: {
+      title: RichText.asText(response.data.title),
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: RichText.asText(response.data.author),
+      content: response.data.group.map(hit => {
+        return {
+          heading: hit.heading[0]?.text ?? ' ',
+          body: hit.body.map(hit => (
+            hit.text
+          )),
+        };
+      })
+    },
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+};
